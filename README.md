3de28ffe88ed94defac0004a3f037c52eb7e8006
ningyao ningshen <ajaxning@gmail.com>
2019-12-24 05:24:35 +0530

    v1

---
+++ README.md
@@ -0,0 +1,7 @@
+`steps 
+    1. python3 -m venv venv
+    2. source venv/bin/activate
+    3. CTRL + SHIFT + P (select venv interpreter)
+    4. pip install speechrecognition
+    5. pip3 install pyaudio
+    
--- main.py
+++ main.py
@@ -1,9 +1,51 @@
 import speech_recognition as sr
+from time import ctime
+import webbrowser
+import time
+
 r = sr.Recognizer()
 
-with sr.Microphone() as source:
-    print("Say something")
-    audio = r.listen(source)
-    voice_data = r.recognize_google(audio)
-    print(voice_data)
-    
+def record_audio(ask = False):
+
+    with sr.Microphone() as source:
+        if ask:
+            print(ask)
+        audio = r.listen(source)
+        voice_data = ''
+        try:
+            voice_data = r.recognize_google(audio)
+            #print(voice_data)
+        except sr.UnknownValueError:
+            print("Sorry, I don't understand what you said.")
+
+        except sr.RequestError:
+            print("Sorry, my speech service is down.")
+
+        return voice_data
+
+
+def respond(voice_data):
+    if 'what is your name' in voice_data:
+        print('My name is text to speech interpreter')
+
+    if 'what is the time' in voice_data:
+        print(ctime())
+    if 'search' in voice_data:
+        search = record_audio('what do you want to search for?')
+        url = 'https://google.com/search?q=' + search
+        webbrowser.get().open(url)
+        print('Here is what if found for ' + search)
+    if 'find location' in voice_data:
+        location = record_audio('what is the location?')
+        url = 'https://google.nl/maps/place/' + location + '/&amp;'
+        webbrowser.get().open(url)
+        print('Here is the location of  ' + location)
+    if 'exit' in voice_data:
+        exit()
+
+time.sleep(1)
+print("How can I help you?")
+while 1:
+    voice_data = record_audio()
+    #print(voice_data)
+    respond(voice_data)
