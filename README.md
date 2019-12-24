630648a6a0ee467d6556fc6624ef197edce2b673
ningyao ningshen <ajaxning@gmail.com>
2019-12-24 06:03:09 +0530

    v2

--- README.md
+++ README.md
@@ -4,4 +4,6 @@
     3. CTRL + SHIFT + P (select venv interpreter)
     4. pip install speechrecognition
     5. pip3 install pyaudio
-    
+    6. pip install gTTS
+    7. pip install playsound
+    8. pip install pyobjc/ python -m easy_install pyobjc
--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import speech_recognition as sr
 from time import ctime
 import webbrowser
 import time
+from playsound import playsound
+import os
+import random
+from gtts import gTTS
+
 
 r = sr.Recognizer()
 
@@ -9,42 +14,55 @@ def record_audio(ask = False):
 
     with sr.Microphone() as source:
         if ask:
-            print(ask)
+            texttospeech_speak(ask)
+            #print(ask)
         audio = r.listen(source)
         voice_data = ''
         try:
             voice_data = r.recognize_google(audio)
             #print(voice_data)
         except sr.UnknownValueError:
-            print("Sorry, I don't understand what you said.")
-
+            #print("Sorry, I don't understand what you said.")
+            texttospeech_speak("Sorry, I don't understand what you said.")
         except sr.RequestError:
-            print("Sorry, my speech service is down.")
-
+            #print("Sorry, my speech service is down.")
+            texttospeech_speak("Sorry, my speech service is down.")
         return voice_data
 
+def texttospeech_speak(audio_string):
+    tts = gTTS(text=audio_string, lang='en')
+    r = random.randint(1, 1000000)
+    audio_file = 'audio-' + str(r) + '.mp3'
+    tts.save(audio_file)
+    playsound(audio_file)
+    #print(audio_string)
+    os.remove(audio_file)
 
 def respond(voice_data):
     if 'what is your name' in voice_data:
-        print('My name is text to speech interpreter')
-
+        #print('My name is text to speech interpreter')
+        texttospeech_speak('My name is text to speech interpreter')
     if 'what is the time' in voice_data:
-        print(ctime())
+        #print(ctime())
+        texttospeech_speak(ctime())
     if 'search' in voice_data:
         search = record_audio('what do you want to search for?')
         url = 'https://google.com/search?q=' + search
         webbrowser.get().open(url)
-        print('Here is what if found for ' + search)
+        #print('Here is what if found for ' + search)
+        texttospeech_speak('Here is what i have found for ' + search)
     if 'find location' in voice_data:
         location = record_audio('what is the location?')
         url = 'https://google.nl/maps/place/' + location + '/&amp;'
         webbrowser.get().open(url)
-        print('Here is the location of  ' + location)
+        #print('Here is the location of  ' + location)
+        texttospeech_speak('Here is the location that you ask for.')
     if 'exit' in voice_data:
+        texttospeech_speak('Stopping application')
         exit()
 
 time.sleep(1)
-print("How can I help you?")
+texttospeech_speak("How can I help you?")
 while 1:
     voice_data = record_audio()
     #print(voice_data)
